07f305cbd06cec8c9a2d56d6019d77532f3f72b2
Serhiy Onyshchenko <serhiy@exalate.com>
2023-10-04 12:48:34 +0000

    RTEAM-651 replaced the httpClient with the JiraClient

---
+++ services/jira/InjectorGetter.groovy
@@ -0,0 +1,17 @@
+package services.jira
+
+import services.SyncScriptContextService
+
+class InjectorGetter {
+    static SyncScriptContextService syncScriptContextService = new SyncScriptContextService()
+
+    static Object getInjector() {
+        def context = syncScriptContextService.syncScriptContext
+        context.injector
+    }
+
+    static def getHttpClient() {
+        def context = syncScriptContextService.syncScriptContext
+        context.httpClient
+    }
+}
--- services/jira/SyncProjectPropertyService.groovy
+++ services/jira/SyncProjectPropertyService.groovy
@@ -1,14 +1,15 @@
 package services.jira
 
 import domain.project_properties.sync.SyncProjectProperty
-import jcloudnode.services.replication.PreparedHttpClient
+import groovy.json.JsonSlurper
 import services.SyncScriptContextService
 
 /**
  * Allows to fetch the project/{projectIdOrKey}/property/sync
  * */
 class SyncProjectPropertyService {
-    private SyncScriptContextService syncScriptContextService
+    private final SyncScriptContextService syncScriptContextService
+    private final JsonSlurper jsonSlurper
 
     SyncProjectPropertyService(SyncScriptContextService syncScriptContextService) {
         this.syncScriptContextService = syncScriptContextService
@@ -16,8 +17,18 @@ class SyncProjectPropertyService {
 
     SyncProjectProperty getSyncProperty(String projectIdOrKey) {
         Map<String, Object> context = syncScriptContextService.syncScriptContext
-        PreparedHttpClient httpClient = context.httpClient
-        Map<String, Object> syncProperty = httpClient.get("/project/${projectIdOrKey}/property/sync".toString()) as Map<String, Object>;
+        Map<String, Object> syncProperty = new JiraClient()
+                .http(
+                        "GET", "/project/${projectIdOrKey}/property/sync".toString(),
+                        [:],
+                        null,
+                        [:]
+                ) { response ->
+                    if (response.code >= 400) throw new com.exalate.api.exception.IssueTrackerException(
+                            "Failed to perform GET /project/${projectIdOrKey}/property/sync : ${response.code} ${response.body}".toString()
+                    )
+                    else jsonSlurper.parseText(response.body)  as Map<String, Object>;
+                } as Map<String, Object>;
         new SyncProjectProperty(
                 syncProperty."connection" as String,
                 syncProperty."active" as String
